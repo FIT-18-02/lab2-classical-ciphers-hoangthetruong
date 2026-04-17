e15f3571c2753f6b0b8a1dc66db0f616907916d6
hoangthetruong <thetruongh36@gmail.com>
2026-04-17 19:09:27 +0700

    Update caesar.cpp

--- src/caesar.cpp
+++ src/caesar.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cctype>   // cho isupper, islower
 using namespace std;
 
 // Caesar Encrypt
@@ -16,13 +17,13 @@ string caesar_encrypt(string text, int key) {
             result += char((ch + key - 'a') % 26 + 'a');
         }
         else {
-            result += ch;  // giữ dấu cách, số, dấu câu...
+            result += ch;   // giữ dấu cách
         }
     }
     return result;
 }
 
-// Caesar Decrypt - TÊN HÀM PHẢI LÀ caesar_decrypt (checker yêu cầu)
+// === HÀM BẮT BUỘC - TÊN ĐÚNG CHECKER ===
 string caesar_decrypt(string text, int key) {
     return caesar_encrypt(text, -key);
 }
@@ -36,9 +37,7 @@ int main() {
     cout << "Original : " << text << endl;
     string enc = caesar_encrypt(text, key);
     cout << "Encrypted: " << enc << endl;
-    cout << "Decrypted: " << caesar_decrypt(enc, key) << endl << endl;
-
-    cout << "Test chữ thường + dấu cách: " << caesar_encrypt("Hello World!", 5) << endl;
+    cout << "Decrypted: " << caesar_decrypt(enc, key) << endl;
 
     return 0;
 }
